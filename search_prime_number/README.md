436eb7d1cf558188fc2a4f48d0506aa9c45a8096
devops-soko <soko.kang@gmail.com>
2021-07-28 21:29:44 +0900

    add bash_script

---
+++ search_prime_number/README.md
@@ -0,0 +1,23 @@
+## Title
+search prime numbers from 1 to n
+
+## Description
+Return the number of prime numbers from 1 to n
+
+## Restriction Conditions
+n is a natural number of more than 2 and less than 1000000.
+
+## I/O Example
+| n | result |
+|:--------|:--------|
+| 10 | 4 | 
+| 5 | 3 | 
+
+## Environment
+OS Version : CentOS 8.3.2011
+
+
+
+
+
+
---
+++ search_prime_number/search_prime_number.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+result=0
+i=$1
+
+if [ $i -ge 2 ] && [ $i -le 1000000 ]
+then
+	while [ $i -ge 2 ]
+	do
+		flag='true'
+		j=2
+		while [ `expr $j \* $j` -le $i ]
+		do
+			if [ `expr $i % $j` -eq 0 ]
+			then
+				flag='false'
+			fi
+			j=`expr $j + 1` 
+		done
+
+		if [ $flag == 'true' ]
+		then
+			result=`expr $result + 1`
+		fi
+		i=`expr $i - 1`
+	done
+fi
+echo $result
+
+exit 0
